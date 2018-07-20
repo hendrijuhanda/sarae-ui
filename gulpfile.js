6ddcc0713274ee1bff5cf72ed7ca0384a17a815b
Hendri Juhanda <hendri.juhanda@gmail.com>
2018-07-20 15:56:03 +0700

    Added nunjucks template builder.

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,9 @@ const gulp = require('gulp')
 const yargs = require('yargs').argv
 const sass = require('gulp-sass')
 const rename = require('gulp-rename')
-const nop = require('gulp-nop')
 const csso = require('gulp-csso')
 const autoprefix = require('gulp-autoprefixer')
-
-gulp.task('default', ['sass'])
-
-gulp.task('watch', ['default'], () => {
-    'use strict'
-
-    gulp.watch('./src/sass/**/*.scss', ['sass'])
-})
+const nunjucksRender = require('gulp-nunjucks-render')
 
 gulp.task('sass', () => {
     'use strict'
@@ -24,12 +16,32 @@ gulp.task('sass', () => {
         .pipe(autoprefix())
 
     if (!yargs.production) {
-        sass_output.pipe(rename('sarae-ui.min.css'))
+        return sass_output.pipe(rename('sarae-ui.min.css'))
         .pipe(gulp.dest('./compiled/css'))
     }
     else {
-        sass_output.pipe(csso())
+        return sass_output.pipe(csso())
         .pipe(rename('sarae-ui.min.css'))
         .pipe(gulp.dest('./dist/css'))
     }
+})
+
+gulp.task('nunjucks', () => {
+    'use strict'
+
+    return gulp.src('./src/nunjucks/**/*.+(html|nunjucks)')
+    .pipe(nunjucksRender({
+        path: ['./src/nunjucks']
+    }))
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('./'))
+})
+
+gulp.task('default', ['sass', 'nunjucks'])
+
+gulp.task('watch', ['default'], () => {
+    'use strict'
+
+    gulp.watch('./src/sass/**/*.scss', ['sass'])
+    gulp.watch('./src/nunjucks/**/*.html', ['nunjucks'])
 })
